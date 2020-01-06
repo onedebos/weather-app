3f88affc0bf8ddb94fe8579904c45a713424607e
Adebola <onedebos@gmail.com>
2020-01-06 21:03:07 +0100

    setup app-logic

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const tempDescription = document.querySelector('.weather-description');
 const noLocationFound = document.querySelector('.current-location');
 const city = document.querySelector('.city');
 const country = document.querySelector('.country');
+const tempBtn = document.querySelector('.temperature');
+
+tempBtn.addEventListener('click', () => {tempBtn.classList.toggle('fahrenheit');
+const cityInput = document.querySelector('#get-location-id').value;
+getWeatherAtLocation(cityInput);
+});
+
+const convertToCelsius = (tempInKelvin) => Math.floor(tempInKelvin + -273.15);
+const convertToFahrenheit = (tempInKelvin) => Math.floor(tempInKelvin + -459.67);
 
 const runForm = () =>{
     const submitForm = document.querySelector('.get-location');
@@ -12,19 +21,19 @@ const runForm = () =>{
         getWeatherAtLocation(cityInput);
         
     })
-}
+};
 
 const grabUserLocation = (lat,lng) =>{
     if(lat == undefined && lng== undefined){
         noLocationFound.innerText = `Click 'Allow' to enable location`;
     }else{
         
-        showWeatherAtUserLocation(lat,lng);
+        showWeatherAtUserLat(lat,lng);
     }
 
 }
 
-const showWeatherAtUserLocation = (lat,lng) => {
+const showWeatherAtUserLat = (lat,lng) => {
     const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=2874e0623c8807994e18916c8cd78f21`;
     fetch (weatherApi, {mode: 'cors'})
       .then((response)=> {return response.json()})
@@ -49,24 +58,33 @@ const getWeatherAtLocation = (inputLocation)=>{
             }else {
                 noLocationFound.innerText = `Weather in ${data.name}`;
             renderData(data);
+            
             }
     });
 
 }
 
 const renderData = (data) =>{
-    
-        tempNum.innerText = Math.floor(data.main.temp + -273.15);
+
+        if(tempBtn.classList.contains('fahrenheit')){
+            tempNum.innerText = convertToFahrenheit(data.main.temp);
+            document.querySelector('.temp-degree').innerText = '';
+            document.querySelector('.temp-celsius').innerText = 'F';
+
+        } else {
+            tempNum.innerText = convertToCelsius(data.main.temp);
+            document.querySelector('.temp-degree').innerText = 'o';
+            document.querySelector('.temp-celsius').innerText = 'C';
+        }
+
         tempDescription.innerText = data.weather[0].description;
-        
         city.innerText = data.name
         country.innerText = data.sys.country;
         noLocationFound.classList.remove('no-location');
         noLocationFound.classList.add('found-location');
-
-        
-    
 }
+
+
 
 const getCurrentLocation = () =>{
         //Get the latitude and the longitude;
@@ -84,5 +102,6 @@ const getCurrentLocation = () =>{
 }
 
 runForm();
-getCurrentLocation();
+// getCurrentLocation();
 grabUserLocation();
+
