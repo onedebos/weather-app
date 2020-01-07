9534d14c94ea9d927dbb1f398dad33364e78621f
Adebola <onedebos@gmail.com>
2020-01-07 10:37:05 +0100

    fix lint errors

--- src/app.js
+++ src/app.js
@@ -1,84 +1,84 @@
-const tempNum = document.querySelector(".temp-num");
-const tempDescription = document.querySelector(".weather-description");
-const noLocationFound = document.querySelector(".current-location");
-const city = document.querySelector(".city");
-const country = document.querySelector(".country");
-const tempBtn = document.querySelector(".temperature");
-const convertToCelsius = tempInKelvin => Math.floor(tempInKelvin + -273.15);
-const convertToFahrenheit = tempInKelvin => Math.floor(tempInKelvin + -459.67);
+const tempNum = document.querySelector('.temp-num');
+const tempDescription = document.querySelector('.weather-description');
+const noLocationFound = document.querySelector('.current-location');
+const city = document.querySelector('.city');
+const country = document.querySelector('.country');
+const tempBtn = document.querySelector('.temperature');
+const convertToCelsius = (tempInKelvin) => Math.floor(tempInKelvin + -273.15);
+const convertToFahrenheit = (tempInKelvin) => Math.floor(tempInKelvin + -459.67);
 
-const renderData = data => {
-  if (tempBtn.classList.contains("fahrenheit")) {
+const renderData = ((data) => {
+  if (tempBtn.classList.contains('fahrenheit')) {
     tempNum.innerText = convertToFahrenheit(data.main.temp);
-    document.querySelector(".temp-degree").innerText = "";
-    document.querySelector(".temp-celsius").innerText = "F";
+    document.querySelector('.temp-degree').innerText = '';
+    document.querySelector('.temp-celsius').innerText = 'F';
   } else {
     tempNum.innerText = convertToCelsius(data.main.temp);
-    document.querySelector(".temp-degree").innerText = "o";
-    document.querySelector(".temp-celsius").innerText = "C";
+    document.querySelector('.temp-degree').innerText = 'o';
+    document.querySelector('.temp-celsius').innerText = 'C';
   }
 
   tempDescription.innerText = data.weather[0].description;
   city.innerText = data.name;
   country.innerText = data.sys.country;
-  noLocationFound.classList.remove("no-location");
-  noLocationFound.classList.add("found-location");
-};
+  noLocationFound.classList.remove('no-location');
+  noLocationFound.classList.add('found-location');
+});
 
-const getWeatherAtLocation = inputLocation => {
+const getWeatherAtLocation = ((inputLocation) => {
   const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&APPID=2874e0623c8807994e18916c8cd78f21`;
-  fetch(weatherApi, { mode: "cors" })
-    .then(response => response.json())
-    .then(data => {
+  fetch(weatherApi, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((data) => {
       if (data.main === undefined) {
-        noLocationFound.innerText = "Location weather unavailable";
-        noLocationFound.classList.remove("found-location");
-        noLocationFound.classList.add("no-location");
+        noLocationFound.innerText = 'Location weather unavailable';
+        noLocationFound.classList.remove('found-location');
+        noLocationFound.classList.add('no-location');
       } else {
         noLocationFound.innerText = `Weather in ${data.name}`;
         renderData(data);
       }
     });
-};
+});
 
-const listenToTempChange = () => {
-  tempBtn.addEventListener("click", () => {
-    tempBtn.classList.toggle("fahrenheit");
-    const cityInput = document.querySelector("#get-location-id").value;
+const listenToTempChange = (() => {
+  tempBtn.addEventListener('click', () => {
+    tempBtn.classList.toggle('fahrenheit');
+    const cityInput = document.querySelector('#get-location-id').value;
     getWeatherAtLocation(cityInput);
   });
-};
+});
 
-const showWeatherAtUserLat = (lat, lng) => {
+const showWeatherAtUserLat = ((lat, lng) => {
   const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=2874e0623c8807994e18916c8cd78f21`;
-  fetch(weatherApi, { mode: "cors" })
-    .then(response => response.json())
-    .then(data => {
+  fetch(weatherApi, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((data) => {
       noLocationFound.innerText = `You're currently in ${data.name}`;
-      noLocationFound.classList.add("found-location");
+      noLocationFound.classList.add('found-location');
       renderData(data);
     });
-};
+});
 
-const runForm = () => {
-  const submitForm = document.querySelector(".get-location");
-  submitForm.addEventListener("submit", event => {
+const runForm = (() => {
+  const submitForm = document.querySelector('.get-location');
+  submitForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const cityInput = document.querySelector("#get-location-id").value;
+    const cityInput = document.querySelector('#get-location-id').value;
     getWeatherAtLocation(cityInput);
   });
-};
+});
 
-const grabUserLocation = (lat, lng) => {
+const grabUserLocation = ((lat, lng) => {
   if (lat === undefined && lng === undefined) {
-    noLocationFound.innerText = "Click 'Allow' to enable location";
+    noLocationFound.innerText = 'Click \'Allow\' to enable location';
   } else {
     showWeatherAtUserLat(lat, lng);
   }
-};
+});
 
-const getCurrentLocation = () => {
-  const successFunction = position => {
+const getCurrentLocation = (() => {
+  const successFunction = (position) => {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     grabUserLocation(lat, lng);
@@ -87,7 +87,7 @@ const getCurrentLocation = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction);
   }
-};
+});
 
 getCurrentLocation();
 runForm();
