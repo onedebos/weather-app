9d12a996329b92a6517ef2b66c5e9cfab036769d
Adebola <onedebos@gmail.com>
2020-01-07 11:09:56 +0100

    fix function lint errors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const noLocationFound = document.querySelector('.current-location');
 const city = document.querySelector('.city');
 const country = document.querySelector('.country');
 const tempBtn = document.querySelector('.temperature');
-const convertToCelsius = (tempInKelvin) => Math.floor(tempInKelvin + -273.15);
-const convertToFahrenheit = (tempInKelvin) => Math.floor(tempInKelvin + -459.67);
+const convertToCelsius = tempInKelvin => Math.floor(tempInKelvin + -273.15);
+const convertToFahrenheit = tempInKelvin => Math.floor(tempInKelvin + -459.67);
 
-const renderData = ((data) => {
+const renderData = data => {
   if (tempBtn.classList.contains('fahrenheit')) {
     tempNum.innerText = convertToFahrenheit(data.main.temp);
     document.querySelector('.temp-degree').innerText = '';
@@ -23,13 +23,13 @@ const renderData = ((data) => {
   country.innerText = data.sys.country;
   noLocationFound.classList.remove('no-location');
   noLocationFound.classList.add('found-location');
-});
+};
 
-const getWeatherAtLocation = ((inputLocation) => {
+const getWeatherAtLocation = inputLocation => {
   const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&APPID=2874e0623c8807994e18916c8cd78f21`;
   fetch(weatherApi, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       if (data.main === undefined) {
         noLocationFound.innerText = 'Location weather unavailable';
         noLocationFound.classList.remove('found-location');
@@ -39,46 +39,46 @@ const getWeatherAtLocation = ((inputLocation) => {
         renderData(data);
       }
     });
-});
+};
 
-const listenToTempChange = (() => {
+const listenToTempChange = () => {
   tempBtn.addEventListener('click', () => {
     tempBtn.classList.toggle('fahrenheit');
     const cityInput = document.querySelector('#get-location-id').value;
     getWeatherAtLocation(cityInput);
   });
-});
+};
 
-const showWeatherAtUserLat = ((lat, lng) => {
+const showWeatherAtUserLat = (lat, lng) => {
   const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=2874e0623c8807994e18916c8cd78f21`;
   fetch(weatherApi, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       noLocationFound.innerText = `You're currently in ${data.name}`;
       noLocationFound.classList.add('found-location');
       renderData(data);
     });
-});
+};
 
-const runForm = (() => {
+const runForm = () => {
   const submitForm = document.querySelector('.get-location');
-  submitForm.addEventListener('submit', (event) => {
+  submitForm.addEventListener('submit', event => {
     event.preventDefault();
     const cityInput = document.querySelector('#get-location-id').value;
     getWeatherAtLocation(cityInput);
   });
-});
+};
 
-const grabUserLocation = ((lat, lng) => {
+const grabUserLocation = (lat, lng) => {
   if (lat === undefined && lng === undefined) {
     noLocationFound.innerText = 'Click \'Allow\' to enable location';
   } else {
     showWeatherAtUserLat(lat, lng);
   }
-});
+};
 
-const getCurrentLocation = (() => {
-  const successFunction = (position) => {
+const getCurrentLocation = () => {
+  const successFunction = position => {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     grabUserLocation(lat, lng);
@@ -87,7 +87,7 @@ const getCurrentLocation = (() => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction);
   }
-});
+};
 
 getCurrentLocation();
 runForm();
